76ce1d014e85a06324ffcb631f5f817d0ec95a1e
Ngoc Truong <tendokuns@gmail.com>
2017-10-09 18:50:31 +0700

    update production

--- config/database.js
+++ config/database.js
@@ -1,4 +1,13 @@
-module.exports = {
+const config = {
 	database: 'mongodb://localhost:27017/voting',
 	secret: 'truongngocsecret020511906'
+};
+
+if (process.env.NODE_ENV === 'production') {
+	config = {
+		database: process.env.DB_URL,
+		secret: process.env.SECRET_KEY
+	}
 }
+
+module.exports = config;
